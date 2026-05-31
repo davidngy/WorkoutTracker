eba77cfadbb8b3d8a11f1a953b36cf44612d67b5
david <davidngy@gmail.com>
2026-05-31 15:08:59 +0200

    set up Express, TypeScript and Drizzle

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "/schema.js";
+if (!process.env.DATABASE_URL) {
+  throw new Error("db url missing")
+}
+
+const client = postgres(process.env.DATABASE_URL);
+export const db = drizzle(client, schema })
---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  email: varchar("email", { length: 256 }).unique().notNull(),
+  hashedPw: varchar("hashed_pw", { length: 256 }).notNull(),
+});
